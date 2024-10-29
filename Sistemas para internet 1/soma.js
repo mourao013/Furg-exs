a3892dc96fe56234fa5dbfd88c7ed12f187412b6
mourao013 <carlosasm013@gmail.com>
2024-10-29 23:25:35 +0000

    resolvidos para entrega

--- Sistemas para internet 1/soma.js
+++ Sistemas para internet 1/soma.js
@@ -4,3 +4,26 @@ function soma(){
     soma = parseInt(x.value) + parseInt(y.value);
     alert(soma);
 }
+function sub(){
+    var x = document.getElementById("v1");
+    var y = document.getElementById("v2");
+    sub = parseInt(x.value) - parseInt(y.value);
+    alert(sub);
+}
+function mult(){
+    var x = document.getElementById("v1");
+    var y = document.getElementById("v2");
+    mult = parseInt(x.value) * parseInt(y.value);
+    alert(mult);
+}
+function div(){
+    var x = document.getElementById("v1");
+    var y = document.getElementById("v2");
+    if (y.value == 0) {
+        alert('impossivel dividir por 0')
+    }
+    else {
+    div = parseInt(x.value) / parseInt(y.value);
+    alert(div);
+    }
+}
