19fa22f64b99fdee847443becb0765a16170f62b
mourao013 <carlosasm013@gmail.com>
2024-10-30 13:58:23 +0000

    exs dia 30 beecrow

--- AED1/auladia30.py
+++ AED1/auladia30.py
@@ -1,3 +1,21 @@
-valores = int(input("Digite:"))
-N = ''
-N2    
+def tira_zero(num):
+	num = str(num)
+	saida = ''
+	for algarismo in num:
+		if algarismo != '0':
+			saida += algarismo
+	return saida
+
+
+def soma_tira_zero():
+	while True:	
+		entrada = input("")
+		entrada = entrada.split(' ')
+		x = int(entrada[0])
+		y = int(entrada[1])
+		soma = x + y
+		if soma == 0:
+			return False
+		print(tira_zero(soma))
+
+soma_tira_zero()
