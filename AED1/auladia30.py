f646d51f829ee1c92541a6bda9d776d8ba7eb1d3
mourao013 <carlosasm013@gmail.com>
2024-10-30 14:29:38 +0000

    exercicios para fazer, 3 e 4

--- AED1/auladia30.py
+++ AED1/auladia30.py
@@ -1,21 +1,27 @@
-def tira_zero(num):
-	num = str(num)
-	saida = ''
-	for algarismo in num:
-		if algarismo != '0':
-			saida += algarismo
-	return saida
+# def tira_zero(num):
+# 	num = str(num)
+# 	saida = ''
+# 	for algarismo in num:
+# 		if algarismo != '0':
+# 			saida += algarismo
+# 	return saida
 
 
-def soma_tira_zero():
-	while True:	
-		entrada = input("")
-		entrada = entrada.split(' ')
-		x = int(entrada[0])
-		y = int(entrada[1])
-		soma = x + y
-		if soma == 0:
-			return False
-		print(tira_zero(soma))
+# def soma_tira_zero():
+# 	while True:	
+# 		entrada = input("")
+# 		entrada = entrada.split(' ')
+# 		x = int(entrada[0])
+# 		y = int(entrada[1])
+# 		soma = x + y
+# 		if soma == 0:
+# 			return False
+# 		print(tira_zero(soma))
 
-soma_tira_zero()
+# soma_tira_zero()
+
+#1 - escreva um programa que leia uma lista de palavras do usuário e retorne outra lista contendo apenas as palabras com mais de 5 caracteres: Feito
+#2 - Escreva um programa que receba uma lista de números e retorne uma nova lista contendo apenas os elementos comuns entre as duas listas: Feito
+#3 - Escreva um programa que recebe duas listas do usuário e retorne uma nova lista contendo apenas os elementos comuns entre as duas listas: Feito
+#4 - Dada uma lista de números inteiros informada pelo usuário, escreva um programa em Python  que conte quantos números únicos (diferentes) estão presentes na lista. A digitação dos elemento da lista deve encerrar quando for digitado o número zero: 
+
