018fcdb60d6b63c3ae04da1467ee08c530ce862f
mourao013 <carlosasm013@gmail.com>
2024-11-12 11:26:28 -0300

    Arrumar essa bomba
    ex4 arrumar

---
+++ AED1/funcao.js
@@ -0,0 +1,18 @@
+function maiormenor(){
+    for(c=0; c<5; c++){
+        var num = prompt("valor: ");
+            var maior = num
+            var menor = num
+        if (num = 0){
+            var maior = num
+            var menor = num
+        }
+        if(num<menor){
+            menor = num
+        }
+        if(num>maior){
+            maior = num
+        }
+    }
+    alert = ("O maior numero e " + maior + "O menor e " + menor)
+    }
